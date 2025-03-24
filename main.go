e1bd8da90460efbef545617932a0c1f81fb9d5ee
Edgaras Kryzevicius <edgaras.kryzevicius@oxylabs.io>
2025-03-24 20:35:07 +0200

    Redis Reader

--- main.go
+++ main.go
@@ -2,38 +2,39 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"os"
 )
 
 func main() {
+	fmt.Println("Listening on port :6379")
+
+	// Create a new server
 	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
+	// Listen for connections
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	defer conn.Close()
 
 	for {
-		buf := make([]byte, 1024)
-
-		// read message from client
-		_, err := conn.Read(buf)
+		resp := NewResp(conn)
+		value, err := resp.Read()
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Println("error reading message from client", err)
-			os.Exit(1)
+			fmt.Println(err)
+			return
 		}
 
-		conn.Write([]byte("+OK\r\n"))
+		fmt.Println(value)
 
+		// ignore request and send back a PONG
+		conn.Write([]byte("+OK\r\n"))
 	}
 }
